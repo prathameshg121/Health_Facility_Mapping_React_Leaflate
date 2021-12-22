684f73e2279d7cc06fa7ff006a0223c08b220b1c
prathameshg121 <ghogarep2000@gmail.com>
2021-12-22 13:05:59 +0530

    update component 1

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About(){
                                
                                 <div class="card"><img class="card-img-top w-100 d-block"/>
                                     <div class="card-body">
-                                        <h4 class="card-title"style={{color:"rgb(228,114,31)"}} >FName LName</h4>
+                                        <h4 class="card-title"style={{color:"rgb(228,114,31)"}} >Prathamesh Ghogare</h4>
                                         <p class="card-text" >191030303<br/>Third Year,<br/>Information Technology, VJTI<br/><br/></p>
                                     </div>
                                 </div>
