9ebca6c3b7515aa4a2bd68628ff972d14e603303
prathameshg121 <ghogarep2000@gmail.com>
2021-12-22 13:31:58 +0530

    ..

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ export default function Home(){
 	<div class="row col-md-10 col-md-offset-1 text-center pad-top">
 		<div class="pad-bottom">
 			<h4>
-            In order to strengthen healthcare delivery and improve business prospects, policy makers, healthcare providers, business leaders, technology providers and pharma companies will need to devise strategies that transform a spark into a sustainable fire. Here are insights from healthcare leaders that can lead India towards a healthy tomorrow. Our aim is provide nearby halth services to the user.
+            In order to strengthen healthcare delivery and improve business prospects, policy makers, healthcare providers, business leaders, technology providers and pharma companies will need to devise strategies that transform a spark into a sustainable fire. Here are insights from healthcare leaders that can lead India towards a healthy tomorrow. This website provide nearby halth services to the user.
 
             </h4></div>
 		
