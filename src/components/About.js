f3489de212a590a1159ce473a874b96466106809
prathameshg121 <ghogarep2000@gmail.com>
2022-01-17 13:22:45 +0530

    ab

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,24 @@ export default function About(){
         <div >
     
         <div class="card-group  about-container">
-                               
                                 <div class="card"><img class="card-img-top w-100 d-block"/>
                                     <div class="card-body">
-                                        <h4 class="card-title"style={{color:"rgb(228,114,31)"}} >Prathamesh Ghogare</h4>
-                                        <p class="card-text" >191030303<br/>Third Year,<br/>Information Technology, VJTI<br/><br/></p>
+                                        <h4 class="card-title" style={{color:"rgb(227,82,82)"}} >Shivam Pawar</h4>
+                                        <p class="card-text" >191080059<br/>Third Year, <br/>Information Technology, VJTI<br/><br/></p>
                                     </div>
                                 </div>
-                               
+                                <div class="card"><img class="card-img-top w-100 d-block"/>
+                                    <div class="card-body">
+                                        <h4 class="card-title"style={{color:"rgb(228,114,31)"}} >Prathmesh Ghogare</h4>
+                                        <p class="card-text" >191080027<br/>Third Year,<br/>Information Technology, VJTI<br/><br/></p>
+                                    </div>
+                                </div>
+                                <div class="card"><img class="card-img-top w-100 d-block"/>
+                                    <div class="card-body">
+                                        <h4 class="card-title" style={{color:"rgb(227,82,82)"}} >Nayan Taori</h4>
+                                        <p class="card-text"><em>191080051</em><br/><em>Third Year,</em><br/><em>Information Technology, VJTI</em><br/><br/></p>
+                                    </div>
+                                </div>
                             </div>
         </div>
     )
